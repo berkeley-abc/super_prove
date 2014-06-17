b382d13a8b16e7e85a1eb448f311a7d93aaaf702
Baruch Sterin <baruchs@gmail.com>
2014-06-16 18:20:35 -0700

    added affinity.py to control cpu affinity of processes

---
+++ affinity.py
@@ -0,0 +1,51 @@
+import os
+
+import ctypes
+import ctypes.util
+
+_number_of_cores = os.sysconf('SC_NPROCESSORS_ONLN')
+
+_libc = ctypes.CDLL(ctypes.util.find_library('c'), use_errno=True)
+_pid_t = ctypes.c_int32
+_mask_t = ctypes.c_uint64
+
+def _or( values ):
+
+    res = 0
+
+    for v in values:
+        res |= v
+
+    return res
+
+def sched_getaffinity(pid):
+
+    mask = _mask_t()
+
+    rc = _libc.sched_getaffinity(
+        ctypes.c_int32(pid),
+        ctypes.sizeof(mask),
+        ctypes.byref(mask)
+    )
+
+    if rc!=0:
+        raise OSError(ctypes.get_errno(), 'sched_getaffinity(): %s'%os.strerror(ctypes.get_errno()))
+
+    mask = mask.value
+    return [ i for i in xrange(_number_of_cores) if mask&(1<<i) ]
+
+
+def sched_setaffinity(pid, cpus):
+
+    mask = _mask_t(_or(1<<c for c in cpus))
+
+    rc = _libc.sched_setaffinity(
+        ctypes.c_int32(pid),
+        ctypes.sizeof(mask),
+        ctypes.byref(mask)
+    )
+
+    if rc!=0:
+        raise OSError(ctypes.get_errno(), 'sched_setaffinity(): %s'%os.strerror(ctypes.get_errno()))
+
+    return mask.value
